9bb510998869a7c20fd47433c638c349e0ffb5e4
Shamany4 <anton-shamov99@mail.ru>
2024-11-19 16:43:43 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const initSlider = () => {
+    const menuLinks = document.querySelectorAll('.rinit-slider-menu > li');
+
+    const parameters = {
+      direction: 'horizontal',
+      slidePerView: 1,
+      slidePerCount: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoHeight: true,
+      loop: false,
+
+      // pagination: {
+      //   el: '.rinit-slider__pagination',
+      //   type: 'fraction',
+      // },
+
+      navigation: {
+        prevEl: '.rinit-slider__button-prev',
+        nextEl: '.rinit-slider__button-next',
+        disabledClass: 'disabled'
+      },
+    }
+
+    const swiper = new Swiper('.rinit-slider', parameters);
+
+    menuLinks.forEach((link, linkIndex) => {
+      link.addEventListener('click', () => {
+        swiper.slideTo(linkIndex + 1, 1000);
+      });
+    });
+  }
+
+  initSlider();
+});
